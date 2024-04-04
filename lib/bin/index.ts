60375601ced7dd0acd218599aa4b71690db94764
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2024-04-04 10:53:08 +0100

    feat: conversion script

--- lib/bin/index.ts
+++ lib/bin/index.ts
@@ -1,15 +1,16 @@
+/* eslint-disable no-console */
 import * as fs from 'fs';
 import * as path from 'path';
 import { shaclStoreToShexSchema, writeShexSchema } from '..';
 import { dereferenceToStore } from '../dereferenceToStore';
 
 async function convertToShex(inPath: string, outPath: string) {
-    const { store, prefixes } = await dereferenceToStore(inPath);
-    const schema = await shaclStoreToShexSchema(store);
-    if (schema.shapes.length === 0) {
-        throw new Error(`No shapes found in ${inPath}`);
-    }
-    fs.writeFileSync(outPath, await writeShexSchema(schema, prefixes));
+  const { store, prefixes } = await dereferenceToStore(inPath);
+  const schema = await shaclStoreToShexSchema(store);
+  if (schema.shapes.length === 0) {
+    throw new Error(`No shapes found in ${inPath}`);
+  }
+  fs.writeFileSync(outPath, await writeShexSchema(schema, prefixes));
 }
 
 const uriInput = /^https?:\/\//.test(process.argv[2]);
@@ -17,21 +18,21 @@ const i = uriInput ? process.argv[2] : path.join(process.cwd(), process.argv[2])
 const o = path.join(process.cwd(), process.argv[3]);
 
 if (uriInput || fs.existsSync(i)) {
-    if (uriInput || fs.statSync(i).isFile()) {
-        // i is a file
-        convertToShex(i, o);
-    } else if (fs.statSync(i).isDirectory()) {
-        // i is a directory
-        // Get all .shaclc files in the shapes directory
-        const shaclcFiles = fs.readdirSync(i);
-        // Process each shacl file
-        shaclcFiles.forEach(file => {
-            const filePath = path.join(i, file);
-            convertToShex(filePath, o).catch(e => console.warn(e));
-        });
-    } else {
-        console.log('Invalid input');
-    }
+  if (uriInput || fs.statSync(i).isFile()) {
+    // i is a file
+    convertToShex(i, o);
+  } else if (fs.statSync(i).isDirectory()) {
+    // i is a directory
+    // Get all .shaclc files in the shapes directory
+    const shaclcFiles = fs.readdirSync(i);
+    // Process each shacl file
+    shaclcFiles.forEach((file) => {
+      const filePath = path.join(i, file);
+      convertToShex(filePath, o).catch((e) => console.warn(e));
+    });
+  } else {
+    console.log('Invalid input');
+  }
 } else {
-    console.log('Input does not exist');
+  console.log('Input does not exist');
 }
