8dec27de6daa72fed161e33a2173dd2331c7dc91
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2024-04-05 16:49:59 +0100

    fix: use non-esm version of term-set

--- __tests__/main-test.ts
+++ __tests__/main-test.ts
@@ -1,1 +1,14 @@
-it('', () => { expect(true).toEqual(true); });
+import * as path from 'path';
+import * as fs from 'fs';
+import { shaclStoreToShexSchema, writeShexSchema } from '../lib';
+import { dereferenceToStore } from '../lib/dereferenceToStore';
+
+it('should be able to convert a shacl shape to a shex shape', async () => {
+  const store = await dereferenceToStore(path.join(__dirname, 'test.shaclc'));
+
+  // Fixme: convert both to shexj and do an equality check on the schema so that
+  // this still passes after minor syntactical changes
+  expect(
+    await writeShexSchema(await shaclStoreToShexSchema(store.store), store.prefixes),
+  ).toEqual(fs.readFileSync(path.join(__dirname, 'test.shex'), 'utf-8'));
+});
