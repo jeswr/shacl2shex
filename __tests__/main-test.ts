f4e7bbacf409b25d6d780e06307bb6fa1a7c0bcb
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2024-04-13 00:46:24 +0100

    chore: cleanup deps

--- __tests__/main-test.ts
+++ __tests__/main-test.ts
@@ -1,14 +1,15 @@
 import * as path from 'path';
 import * as fs from 'fs';
+import dereferenceToStore from 'rdf-dereference-store';
+import type { Store } from 'n3';
 import { shaclStoreToShexSchema, writeShexSchema } from '../lib';
-import { dereferenceToStore } from '../lib/dereferenceToStore';
 
 it('should be able to convert a shacl shape to a shex shape', async () => {
-  const store = await dereferenceToStore(path.join(__dirname, 'test.shaclc'));
+  const store = await dereferenceToStore(path.join(__dirname, 'test.shaclc'), { localFiles: true });
 
   // Fixme: convert both to shexj and do an equality check on the schema so that
   // this still passes after minor syntactical changes
   expect(
-    await writeShexSchema(await shaclStoreToShexSchema(store.store), store.prefixes),
+    await writeShexSchema(await shaclStoreToShexSchema(store.store as Store), store.prefixes),
   ).toEqual(fs.readFileSync(path.join(__dirname, 'test.shex'), 'utf-8'));
 });
