c930c9e20e774d77bb7cf23c9c8a5abe82586e2a
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2025-07-27 13:03:26 +0100

    feat: support nodeKind on nodeShapes (#333)

--- __tests__/main-test.ts
+++ __tests__/main-test.ts
@@ -4,11 +4,11 @@ import dereferenceToStore from 'rdf-dereference-store';
 import { shaclStoreToShexSchema, writeShexSchema } from '../lib';
 
 const files = fs.readdirSync(path.join(__dirname))
-  .filter((file) => file.endsWith('.shaclc'));
+  .filter((file) => file.endsWith('.shaclc') || file.endsWith('.shce'));
 
 it.each(files)('should convert %s', async (file) => {
   const store = await dereferenceToStore(path.join(__dirname, file), { localFiles: true });
-  const expectedShex = fs.readFileSync(path.join(__dirname, file.replace('.shaclc', '.shex')), 'utf-8');
+  const expectedShex = fs.readFileSync(path.join(__dirname, file.replace(/\.(shaclc|shce)$/, '.shex')), 'utf-8');
   const shexSchema = await shaclStoreToShexSchema(store.store);
   await expect(writeShexSchema(shexSchema, store.prefixes)).resolves.toEqual(expectedShex);
 });
